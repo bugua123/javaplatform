6b0c110872946e08e95694640cbbd510d6fdc858
bugua123 <47909592@163.com>
2020-01-08 09:08:51 +0800

    在menuServiceImpl添加缓存支持，并将ehcache.xsd保存到本地加载，防止网络加载过慢问题

--- src/main/java/com/wzw/platform/PlatformApplication.java
+++ src/main/java/com/wzw/platform/PlatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cache.annotation.EnableCaching;
-
+@EnableCaching
 @SpringBootApplication
 @MapperScan("com.wzw.platform.dao")
 public class PlatformApplication {
--- src/main/java/com/wzw/platform/service/impl/MenuServiceImpl.java
+++ src/main/java/com/wzw/platform/service/impl/MenuServiceImpl.java
@@ -6,12 +6,16 @@ import com.wzw.platform.model.entity.MenuEntity;
 import com.wzw.platform.service.MenuService;
 import com.wzw.platform.utils.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CacheConfig(cacheNames = "codeCache")
 @Service("menuServiceImpl")
 public class MenuServiceImpl implements MenuService {
 
@@ -20,6 +24,7 @@ public class MenuServiceImpl implements MenuService {
     @Autowired
     private RoleDao roleDao;
 
+    @Cacheable(key = "#p0") //添加缓存
     @Override
     public List<MenuEntity> menuList(int id) {
         List<String> idList = roleDao.getModulesById(id);
@@ -61,12 +66,12 @@ public class MenuServiceImpl implements MenuService {
     public void insertMenu(MenuEntity menuEntity) {
         menuDao.insertMenu(menuEntity);
     }
-
+    @CachePut(key = "#p0")
     @Override
     public void updateMenu(MenuEntity menuEntity) {
         menuDao.updateMenu(menuEntity);
     }
-
+    @CacheEvict(key = "#p0")
     @Override
     public void deleteMenus(List<String> groupId) {
         menuDao.deleteMenus(groupId);
